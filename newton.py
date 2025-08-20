974f7c7ade06c69f276937c0c12af454228d5042
cjshen779 <cjshen@berkeley.edu>
2025-08-20 10:23:00 -0700

    Add newton.py

---
+++ newton.py
@@ -0,0 +1,27 @@
+import math 
+import numpy as np 
+
+epsilon = 0.001 
+delta = 0.0001 
+
+def first_deriv(f, x, delta): 
+    result = (f(x+delta) - f(x))/delta  
+    return result 
+    
+def second_deriv(f, x, delta): 
+    result = (first_deriv(f, x + delta, delta) - first_deriv(f, x, delta))/delta 
+    return result 
+
+def optimize(x_0, f): 
+    # initial vals 
+    x_vals = [x_0] 
+    x_t = x_0  
+    
+    dist = math.inf   
+    
+    while dist > epsilon: 
+        x_t = x_t - (first_deriv(f, x_t, delta)/second_deriv(f,x_t, delta)) 
+        dist = abs(x_t - x_vals[-1]) 
+        x_vals.append(x_t)
+
+    return f"(x, f(x)) = {(x_t, f(x_t))}" 
